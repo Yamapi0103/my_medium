e42a8511d5cdb94f4a38f981a16f0d6de7e4c720
JoshuaYeh <yamapi0103@gmail.com>
2022-07-02 21:52:32 +0800

    aasm 狀態機

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,6 @@
 class Story < ApplicationRecord
+  include AASM
+
   belongs_to :user
   validates :title, presence: true
   default_scope { where(deleted_at: nil) }
@@ -6,5 +8,22 @@ class Story < ApplicationRecord
   def destroy
     update(deleted_at: Time.now)
   end
-  
+
+  aasm(column: 'status', no_direct_assignment: true ) do
+    state :draft, initial: true
+    state :published
+
+    event :publish do
+      transitions from: :draft, to: :published
+      after do
+        puts '上架成功！'
+      end
+    end
+
+    event :unpublish do
+      transitions from: :published, to: :draft
+    end
+
+  end
+
 end
