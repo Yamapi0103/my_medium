54281db51c685b985dd744320a2f835eb5e483ac
JoshuaYeh <yamapi0103@gmail.com>
2022-07-03 16:01:47 +0800

    幫文章加上網址

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'devise', '~> 4.8', '>= 4.8.1'
 gem 'aasm', '~> 4.11'
+gem 'friendly_id', '~> 5.4', '>= 5.4.2'
+gem 'babosa', '~> 2.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,7 @@
 class Story < ApplicationRecord
+  extend FriendlyId
+  friendly_id :slug_candidate, use: :slugged
+
   include AASM
 
   belongs_to :user
@@ -24,6 +27,19 @@ class Story < ApplicationRecord
       transitions from: :published, to: :draft
     end
 
+  end
+
+  def normalize_friendly_id(input)
+    input.to_s.to_slug.normalize(transliterations: :russian).to_s
+  end
+
+  private 
+  
+  def slug_candidate
+    [
+      :title,
+      [:title, SecureRandom.hex[0, 8]]
+    ]
   end
 
 end
